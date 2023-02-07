7577f78e74f591e2d426bbb78484ffc5c53a6df2
JamiYashwanth <jamiyashwanth18@gmail.com>
2023-02-07 23:15:04 +0530

    Excel sheet generation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+from scraping import *
+import time
 TOKEN = 'MTA3MjIwMzM4OTEyMDgyMzI5Ng.GBYYob.sK_gCGdfNLv3bYGCVEIUjpjeT00VN4hf3z_a88'
 #
 client = discord.Client(intents=discord.Intents.default())
@@ -10,8 +12,14 @@ async def on_message(message):
     if user_message == 'Hi':
         await message.channel.send(f'Hello {username}')
         return
-    if user_message == 'ranking':
-        await message.channel.send('Will be soon...')
+    if user_message.startswith('rankings'):
+        await message.channel.send('Generating Excel sheet , Please wait for some time....')
+        contest_id = user_message.split(' ')[1].upper()
+        scrape(contest_id,'A')
+        scrape(contest_id,'B')
+        scrape(contest_id,'C')
+        scrape(contest_id,'D')
+        await message.channel.send(file=discord.File(f'{contest_id}_rankings.xlsx'))
         return
 
 client.run(TOKEN)
--- scraping.py
+++ scraping.py
@@ -1,35 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-from openpyxl import Workbook
+from openpyxl import Workbook,load_workbook
 from bs4 import BeautifulSoup
 
 
-url = "https://www.codechef.com/rankings/START76B?filterBy=Institution%3DVignan%27s%20Institute%20Of%20Information%20Technology%2C%20Visakhapatnam&itemsPerPage=100&order=asc&page=1&sortBy=rank"
 
-# initiating the webdriver. Parameter includes the path of the webdriver.
-driver = webdriver.Chrome('./chromedriver')
-driver.get(url)
-time.sleep(10)
+def scrape(contest_id,div):
+    url = f'https://www.codechef.com/rankings/{contest_id + div}?filterBy=Institution%3DVignan%27s%20Institute%20Of%20Information%20Technology%2C%20Visakhapatnam&itemsPerPage=100&order=asc&page=1&sortBy=rank'
+    op = webdriver.ChromeOptions()
+    op.add_argument('headless')
+    driver = webdriver.Chrome('./chromedriver',options=op)
+    driver.get(url)
+    time.sleep(5)
 
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-u=soup.find_all('tr',class_='MuiTableRow-root')
-user_data=[]
-for user_list in u:
-    user = []
-    username = user_list.find('span',class_="m-username--link")
-    userRank = user_list.find('p',class_="MuiTypography-root")
-    if((username != None)):
-        user.append(username.text)
-        user.append(userRank.text)
-        user_data.append(user)
-print(user_data)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    u = soup.find_all('tr', class_='MuiTableRow-root')
+    user_data = []
+    for user_list in u:
+        user = []
+        username = user_list.find('span', class_="m-username--link")
+        userRank = user_list.find('p', class_="MuiTypography-root")
+        row = user_list.find_all('td', class_="MuiTableCell-root")
+        if ((username != None)):
+            user.append(userRank.text)  # rank
+            user.append(username.text)  # username
+            user.append((row[2].text)[11:])  # total score
+            user.append((row[3].text)[7:])  # total time
+            user_data.append(user)
 
-wb=Workbook()
-ws = wb.active
+    try:
+        wb=load_workbook(f'{contest_id}_rankings.xlsx')
+    except:
+        wb = Workbook()
+        wb.save(f'{contest_id}_rankings.xlsx')
+    try:
+        wb[f'Div {div}']
+    except:
+        wb.create_sheet(f'Div {div}')
+    ws = wb[f'Div {div}']
+    ws.delete_rows(0, ws.max_row - 1)
+    row = ['Rank', 'Username', 'Total score', 'Total time']
+    ws.append(row)
+    if len(user_data) > 0:
+        for data in user_data:
+            ws.append(data)
+    wb.save(filename=f'{contest_id}_rankings.xlsx')
 
-for data in user_data:
-    ws.append(data)
 
-wb.save("rankings.xlsx")
